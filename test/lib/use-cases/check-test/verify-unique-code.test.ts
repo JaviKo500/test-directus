058ecd4c0cf3bf05b81a97553911146e3c98ea11
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-27 10:24:04 -0500

    test: verify unique code test

--- test/lib/use-cases/check-test/verify-unique-code.test.ts
+++ test/lib/use-cases/check-test/verify-unique-code.test.ts
@@ -1,11 +1,51 @@
+import { DirectusErrors } from '../../../../src/lib/config/adapters/directus-errors';
+import { COLLECTION_TEST } from '../../../../src/lib/config/collections';
 import { Context } from '../../../../src/lib/interfaces';
 import { verifyUniqueCode } from '../../../../src/lib/use-cases/check-test/verify-unique-code';
 import { Config } from '../../../../src/lib/use-cases/config';
+import { test } from 'vitest';
+
+vi.mock('../../../../src/lib/config/adapters/directus-errors');
+
 
 describe('Verify-unique-code.test', () => {
   let context: Context = {};
-  test( 'test', async () => {
+  beforeAll( async () => {
     const config = Config.getInstance();
     context = await config.getContext();
+  });
+  test( 'should throw error if code is empty', async () => {
+    const test = {
+      code: ''
+    };
+
+    await expect( verifyUniqueCode( test, context ) ).rejects.toThrow();
+    expect( DirectusErrors.invalidPayload ).toHaveBeenCalledWith(
+      'test code is required',
+      COLLECTION_TEST,
+      'code '
+    );
+  });
+
+  test( 'should throw error if code exists', async () => {
+    const test = {
+      code: 'test12',
+      name: 'test12'
+    };
+
+    await expect( verifyUniqueCode( test, context ) ).rejects.toThrow();
+    expect( DirectusErrors.invalidPayload ).toHaveBeenCalledWith(
+      'test code exists',
+      COLLECTION_TEST,
+      'code '
+    );
+  });
+
+  test( 'should not throw error if code is unique', async () => {
+    const test = {
+      code: 'unique-code',
+      name: 'unique code'
+    };
+    await expect( verifyUniqueCode( test, context ) ).resolves.not.toThrow();
   });
 });
